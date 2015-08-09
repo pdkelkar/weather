fea51b1ac131f8088ed62b0ed161ea341750fc88
priyangithub <priyadarshan.kelkar@gmail.com>
2015-08-09 12:54:30 +1000

    Application Exception
    New custom exception for city id not valid

---
+++ src/main/java/au/com/pactera/code/test/exception/CityIdNotFoundException.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package au.com.pactera.code.test.exception;
+
+/**
+ * @author Priyadarshan
+ *
+ */
+public class CityIdNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3359392070538400861L;
+
+	/**
+	 * 
+	 */
+	public CityIdNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public CityIdNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public CityIdNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public CityIdNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public CityIdNotFoundException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
