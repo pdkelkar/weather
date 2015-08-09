407fa20af70e2f65a91980184efd5aff917ca78a
priyangithub <priyadarshan.kelkar@gmail.com>
2015-08-09 12:54:48 +1000

    Updated method signature
    Updated method signature

--- src/main/java/au/com/pactera/code/test/service/WeatherService.java
+++ src/main/java/au/com/pactera/code/test/service/WeatherService.java
@@ -1,5 +1,6 @@
 package au.com.pactera.code.test.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -7,11 +8,12 @@ import org.springframework.stereotype.Service;
 
 import au.com.pactera.code.test.dto.City;
 import au.com.pactera.code.test.dto.WeatherInfo;
+import au.com.pactera.code.test.exception.CityIdNotFoundException;
 
 @Service
 public interface WeatherService {
 	
-	public WeatherInfo getWeather(String cityId) throws Exception;
+	public WeatherInfo getWeather(String cityId) throws IOException, CityIdNotFoundException;
 	
 	public List<City> getCities();
 	
