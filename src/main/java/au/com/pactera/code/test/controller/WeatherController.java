34a206c00f72078f3a31127c53f41ca982beff68
priyangithub <priyadarshan.kelkar@gmail.com>
2015-08-09 12:54:05 +1000

    Exception Handling
    added exception handling for cityId not valid condition

--- src/main/java/au/com/pactera/code/test/controller/WeatherController.java
+++ src/main/java/au/com/pactera/code/test/controller/WeatherController.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import au.com.pactera.code.test.dto.City;
 import au.com.pactera.code.test.dto.WeatherInfo;
+import au.com.pactera.code.test.exception.CityIdNotFoundException;
 import au.com.pactera.code.test.service.WeatherService;
 
 /**
@@ -29,6 +34,10 @@ public class WeatherController{
 	
     @Autowired
     private WeatherService weatherService;
+    
+	@Value("${error.cityId}")
+	private String cityIdError;
+
 	
 	@RequestMapping(value = "/cities", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
@@ -40,20 +49,36 @@ public class WeatherController{
 
 	@RequestMapping(value = "/today", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public WeatherInfo getCityWeather(@RequestBody String cityId) {
+	public WeatherInfo getCityWeather(@RequestBody String cityId) throws CityIdNotFoundException {
 		logger.info("getCityWeather()-Entry");
 		WeatherInfo currentWeather = new WeatherInfo();
 		try {
 			currentWeather = weatherService.getWeather(cityId);
 		} catch (IOException e) {
 			logger.error("IOException occured for getWeather() service"+ e);
-		}
-		catch (Exception e) {
-			logger.error("General Exception occured for getWeather() service"+ e);
-		}
+		} 
 		logger.info("getCityWeather()-Exit");
 		return currentWeather;
+	}
+	
+	  @ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="${error.cityId}")  // 400
+	  @ExceptionHandler(CityIdNotFoundException.class)
+	  public void cityIdError() {
+	    logger.error(getCityIdError());
+	  }
 
+	/**
+	 * @return the cityIdError
+	 */
+	public String getCityIdError() {
+		return cityIdError;
+	}
+
+	/**
+	 * @param cityIdError the cityIdError to set
+	 */
+	public void setCityIdError(String cityIdError) {
+		this.cityIdError = cityIdError;
 	}
 
 }
